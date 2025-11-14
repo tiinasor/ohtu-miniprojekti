b54fae5a485081e4bfe159f180e56bdcf0af780f
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-11-14 22:35:30 +0200

    edit boilerplate database setup function to create citation sql table instead of todo table

--- src/repositories/citation_repository.py
+++ src/repositories/citation_repository.py
@@ -8,7 +8,7 @@ def get_citations():
     citations = result.fetchall()
     return [Citation(citation[0], citation[1], citation[2], citation[3], citation[4], citation[5], citation[6], citation[7], citation[8], citation[9]) for citation in citations] 
 
-def create_citation(name: str, citation_type="article", author: str, title: str, journal: str, year: int, volume: float, number: int, pages: str):
+def create_citation(name: str, citation_type: str, author: str, title: str, journal: str, year: int, volume: float, number: int, pages: str):
     sql = text("INSERT INTO citations (name, citation_type, author, title, journal, year, volume, number, pages) VALUES (:name, :citation_type, :author, :title, :journal, :year, :volume, :number, :pages)")
     db.session.execute(sql, { "name": name, "citation_type": citation_type, "author": author, "title": title, "journal": journal, "year": year, "volume": volume, "number": number, "pages": pages })
     db.session.commit()
--- src/schema.sql
+++ src/schema.sql
@@ -1,5 +1,12 @@
-CREATE TABLE todos (
-  id SERIAL PRIMARY KEY, 
-  content TEXT NOT NULL,
-  done BOOLEAN DEFAULT FALSE
-)
+CREATE TABLE citations (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  citation_type TEXT NOT NULL,
+  author TEXT NOT NULL,
+  title TEXT NOT NULL,
+  journal TEXT NOT NULL,
+  year INTEGER NOT NULL,
+  volume FLOAT NOT NULL,
+  number INTEGER NOT NULL,
+  pages TEXT NOT NULL
+);
