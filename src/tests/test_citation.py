43a777a33e235748f031c72402db0cacaad89a87
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-12-07 22:45:49 +0200

    Remove redundancy from citation unittests

--- src/tests/test_citation.py
+++ src/tests/test_citation.py
@@ -6,49 +6,28 @@ from entities.citation import Citation
 os.environ["TEST_ENV"] = "true"
 
 class TestCitation(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
 
-    def test_citation_creation(self):        
-        ref_info = [1,
-                    "example-citation",
-                    "book",
-                    "example address",
-                    "example author", 
-                    "example exambooktitle", 
-                    "example edition", 
-                    "example editor", 
-                    "example howpublished", 
-                    "example journal",
-                    "example keywords",
-                    "example month", 
-                    "example note", 
-                    "example number", 
-                    "example organization", 
-                    "example pages", 
-                    "example publisher", 
-                    "example school", 
-                    "example series", 
-                    "example title", 
-                    "example type", 
-                    "example volume", 
-                    "example year"] 
-        citation = Citation(ref_info)
+    def ref_info(self):
+        return [1,
+                "example-name",
+                "misc",
+                "example address",
+                "example author", 
+                "example booktitle"] + [None for _ in range(23)]
+
+    def test_citation_creation(self):
+        citation = Citation(self.ref_info())
         self.assertEqual(citation.id, 1)
         self.assertEqual(citation.get_field("id"), 1)
-        self.assertEqual(citation.get_field("name"), "example-citation")
-        self.assertEqual(citation.get_field("citation_type"), "book")
+        self.assertEqual(citation.get_field("name"), "example-name")
+        self.assertEqual(citation.get_field("citation_type"), "misc")
         self.assertEqual(citation.get_field("author"), "example author")
-        self.assertEqual(citation.get_field("title"), "example title")
-        self.assertEqual(citation.get_field("journal"), "example journal")
-        self.assertEqual(citation.get_field("year"), "example year")
+
 
     def test_get_invalid_field(self):
-        ref_info = [2, "example-citation"] + [None for _ in range(23)]
-        citation = Citation(ref_info)
+        citation = Citation(self.ref_info())
         self.assertRaises(ValueError, citation.get_field, "non_existent_field")
 
 if __name__ == "__main__":
     unittest.main()
+
